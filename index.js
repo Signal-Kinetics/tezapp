05a23bd5d5466fe1bd0672f2ffbc823a3e3641a2
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-07-08 12:39:16 +0800

    🎛️   GSCA-8/9 => Showing & populating contract interface with user metadata from contract

--- index.js
+++ index.js
@@ -2,3 +2,43 @@ import { BeaconWallet } from "@taquito/beacon-wallet";
 import { Tezos } from "@taquito/taquito";
 
 const contractAddress = "";
+
+window.onload = () => {
+    document.getElementById("init-wallet").onclick = initWallet;
+};
+
+const updateInnerText = (id, text) =>
+    (document.getElementById(id).innerText = text);
+
+const initWallet = async () => {
+    try {
+        Tezos.setProvider({ rpc: `https://carthagenet.SmartPy.io` });
+        const options = {
+            name: "Beacon SDK Integration"
+        };
+        const wallet = new BeaconWallet(options);
+        const network = {
+            type: "carthagenet"
+        };
+        await wallet.requestPermissions({ network });
+        Tezos.setWalletProvider(wallet);
+
+        // Update dapp interface with user metadata
+        userAddress = wallet.permissions.address;
+        userBalance = await Tezos.tz.getBalance(userAddress);
+        contractInstance = await Tezos.wallet.at(contractAddress);
+        const storage = await cntractInstance.storage();
+
+        // Hides "connect" button
+        document.getElementById("connection").style.display = "none";
+
+        // Shows and populates the contract interface
+        document.getElementById("interface").style.display = "block";
+        updateInnerText("user-address", userAddress);
+        updateInnerText("user-balance", userBalance / 1000000);
+        updateInnerText("current-message", storage.message);
+        updateInnerText("current-value", storage.integer);
+    } catch (error) {
+        console.log(error);
+    }
+}
